34639b783a7bb6822288b35ad61fb1fcb679ec65
Breeana Payton <breeana.payton@gmail.com>
2021-12-27 18:25:57 -0600

    embeded calendly calendar

--- src/Components/Contact/index.tsx
+++ src/Components/Contact/index.tsx
@@ -1,6 +1,5 @@
 import { Grid, Box } from "@material-ui/core";
-import { Mail } from "@material-ui/icons";
-import ContactForm from "./ContactForm";
+import { InlineWidget } from "react-calendly";
 import styles from "./styles";
 
 const Contact = () => {
@@ -9,30 +8,7 @@ const Contact = () => {
   return (
     <section id="Contact" className={classes.formContainer}>
       <header className={classes.header}>Contact</header>
-
-      <Grid container item justifyContent="center" alignItems="center">
-        <Grid item xs={5}>
-          <Box className={classes.contactCTA}>
-            What Can I Do For You? <br /> <br />
-            <span className={classes.contactEmoji}>😊</span>
-          </Box>
-          <Grid
-            container
-            justifyContent="center"
-            alignContent="center"
-            item
-            className={classes.text}
-          >
-            <Mail fontSize="large" className={classes.mailIcon} />
-            <Grid item>
-              <p className={classes.email}>breeana.payton@gmail.com</p>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <ContactForm />
-        </Grid>
-      </Grid>
+      <InlineWidget url="https://calendly.com/breeana-payton/30min?month=2021-12" />
     </section>
   );
 };
