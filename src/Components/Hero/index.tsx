77d702018ca8b3aa7b06936b5ff3960f18bbd602
Breeana Payton <breeana.payton@gmail.com>
2022-04-01 21:01:40 -0400

    added formspree package and updated resume link

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -5,68 +5,74 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import styles from "./styles";
 
 const Hero = () => {
-  const classes = styles();
+    const classes = styles();
 
-  return (
-    <section id="Home">
-      <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
-        className={classes.heroSection}
-        spacing={0}
-      >
-        <Grid container item justifyContent="center">
-          <Box className={classes.heroText}>
-            <header id="hero-text">
-              Hello 👋, I'm Bree Payton <br /> A Front-End Engineer
-            </header>
-          </Box>
+    return (
+        <section id="Home">
+            <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                className={classes.heroSection}
+                spacing={0}
+            >
+                <Grid container item justifyContent="center">
+                    <Box className={classes.heroText}>
+                        <header id="hero-text">
+                            Hello 👋, I'm Bree Payton <br /> A Front-End Engineer
+                        </header>
+                    </Box>
 
-          <Grid container item justifyContent="center" className={classes.btnContainer} spacing={3}>
-            <Grid item>
-              <Button
-                className={classes.button}
-                variant="outlined"
-                size="large"
-                href="https://github.com/PaytonDev"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <GitHubIcon className={classes.icon} />
-                Github
-              </Button>
+                    <Grid
+                        container
+                        item
+                        justifyContent="center"
+                        className={classes.btnContainer}
+                        spacing={3}
+                    >
+                        <Grid item>
+                            <Button
+                                className={classes.button}
+                                variant="outlined"
+                                size="large"
+                                href="https://github.com/PaytonDev"
+                                rel="noreferrer"
+                                target="_blank"
+                            >
+                                <GitHubIcon className={classes.icon} />
+                                Github
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                className={classes.button}
+                                variant="outlined"
+                                size="large"
+                                href="https://www.linkedin.com/in/breeana-payton/"
+                                rel="noreferrer"
+                                target="_blank"
+                            >
+                                <LinkedInIcon className={classes.icon} />
+                                LinkedIn
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                className={classes.button}
+                                variant="outlined"
+                                href="https://drive.google.com/file/d/1bu7qBWP1oQbWL-VoLvQtQb6WiiXJxb5r/view?usp=sharing"
+                                target="_blank"
+                                size="large"
+                            >
+                                <DescriptionIcon className={classes.icon} />
+                                Resume
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Grid>
-            <Grid item>
-              <Button
-                className={classes.button}
-                variant="outlined"
-                size="large"
-                href="https://www.linkedin.com/in/breeana-payton/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <LinkedInIcon className={classes.icon} />
-                LinkedIn
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button
-                className={classes.button}
-                variant="outlined"
-                href="https://docs.google.com/document/d/1WvQkP8JxqTqDpqm9lX4cCDlfiAvc1SlvPrx4VWiYsBA/edit?usp=sharing"
-                target="_blank"
-                size="large"
-              >
-                <DescriptionIcon className={classes.icon} />
-                Resume
-              </Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Hero;
